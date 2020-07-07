df705bd275a5e5acea37d4d724e6cc7a263a36cd
Kaue Capris <kaue@hotmail.it>
2020-07-07 04:17:21 -0300

    Add project files.

---
+++ FirstCodetoGit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FirstCodetoGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
