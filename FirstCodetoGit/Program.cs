0b03a0011712d66000190d797cf4b76f510bdb6d
Kaue Capris <kaue@hotmail.it>
2020-07-07 04:23:16 -0300

    first Commit

--- FirstCodetoGit/Program.cs
+++ FirstCodetoGit/Program.cs
@@ -6,7 +6,7 @@ namespace FirstCodetoGit
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Git!");
         }
     }
 }
